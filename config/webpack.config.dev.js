b3a3deb0366bc39003cbb6d96a3c69172ef8fc31
Matheus Mesquita <matheusmesquita.168@gmail.com>
2017-05-03 17:42:36 -0300

    added sass loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -113,7 +113,8 @@ module.exports = {
           /\.(js|jsx)(\?.*)?$/,
           /\.css$/,
           /\.json$/,
-          /\.svg$/
+          /\.svg$/,
+          /\.sass/
         ],
         loader: 'url',
         query: {
@@ -156,9 +157,13 @@ module.exports = {
         query: {
           name: 'static/media/[name].[hash:8].[ext]'
         }
-      }
+      },
       // ** STOP ** Are you adding a new loader?
       // Remember to add the new extension(s) to the "url" loader exclusion list.
+      {
+        test: /\.sass$/,
+        loader: 'style!css?importLoaders=1!postcss!sass'
+      }
     ]
   },
   
