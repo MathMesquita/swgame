41486b080cba3a1811a80796250bdef8368ccc9a
Matheus Mesquita <matheusmesquita.168@gmail.com>
2017-05-03 17:42:16 -0300

    solved airbnb eslint errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './index.sass';
+
+const document = global.document;
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
