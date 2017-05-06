712e6350395172b83c2c3541760177929cc9046d
Matheus Mesquita <matheusmesquita.168@gmail.com>
2017-05-06 12:14:31 -0300

    Updates

---
+++ static/js/polyfills.5d0918f6.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],[function(e,n,i){"use strict";"undefined"==typeof Promise&&(i(108).enable(),window.Promise=i(107)),i(190),Object.assign=i(4)}]);
+//# sourceMappingURL=polyfills.5d0918f6.js.map
